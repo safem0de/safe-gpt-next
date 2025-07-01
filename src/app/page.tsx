8173fe08ef9dbe91b5063b3c8e17250d83c9eb1c
Naweeparb <naweeparbw@borneothai.com>
2025-07-01 17:02:21 +0700

    Rename missing named

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { connectMongoDB } from "@/config/database";
-import ChatArea from "@/components/ChartArea";
+import ChatArea from "@/components/ChatArea";
 
 // connectMongoDB();
 export default function page() {
